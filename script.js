effcb3023e933c4efd1464a5ebcfdcd61a01cfb6
omar2340 <omarfugar20@gmail.com>
2024-11-25 23:18:37 -0500

    Actualización del formulario

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-document.querySelector("form").addEventListener("submit", function(event) {
-    const carrera = document.getElementById("carrera").value;
-    if (carrera === "") {
-        alert("Por favor selecciona una carrera antes de continuar.");
-        event.preventDefault(); // Esto evita que se envíe el formulario
-    } else {
-        alert("Formulario enviado correctamente.");
-    }
+document.querySelector("form").addEventListener("submit", function(e) {
+    alert("Formulario enviado exitosamente");
 });
